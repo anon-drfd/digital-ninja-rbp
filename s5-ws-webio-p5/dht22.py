e520a6b925601573e4f4a1b73578e5361a3ad2a1
anon-drfd <53021783+anon-drfd@users.noreply.github.com>
2020-09-10 18:16:22 +0800

    Add files via upload

--- s5-ws-webio-p5/dht22.py
+++ s5-ws-webio-p5/dht22.py
@@ -3,24 +3,23 @@ dht22.py
 Temperature/Humidity monitor using Raspberry Pi and DHT22. 
 Data is displayed on thingspeak.com
 """ 
+import sys 
 from time import sleep 
 import Adafruit_DHT as dht
 import requests
 
-DHT_PIN = 4 #BCM numbering of the GPIO pins.
-DHT_SENSOR = dht.DHT22 #Sensor used
-
-# Enter Your API key here
-myAPI = "0QLCT2JSC02AYONS"
 
 # Reading from DHT22 and storing the temperature and humidity
 def getSensorData():
+    DHT_PIN = 4 #BCM numbering of the GPIO pins.
+    DHT_SENSOR = dht.DHT22 #Sensor used
     #Get sensor data
     hmdty, temp = dht.read_retry(DHT_SENSOR, DHT_PIN)
     return (hmdty, temp)
 
 def uploadSensorData():
-    print("Starting...")
+    # Enter Your API key here
+    myAPI = "0QLCT2JSC02AYONS"
     #This is the URL used to send the sensor reading to ThingSpeak (Do not change)
     baseURL = "https://api.thingspeak.com/update?api_key=%s" % myAPI 
     while True:
@@ -35,12 +34,16 @@ def uploadSensorData():
                 temp = '%.2f' % temp
 
                 # Sending the data to thingspeak
-                conn = requests.get(baseURL + '&field1=%s&field2=%s' % (hmdty, temp))                
-                conn.close() # Closing the connection
+                conn = requests.get(baseURL + '&field1=%s&field2=%s' % (hmdty, temp))                # Closing the connection
+                conn.close()
             # DHT22 requires 2 seconds to give a reading, so make sure to add delay of above 2 seconds.
-            sleep(20)
+            sleep()
             
         except:
             break
+        
+# call main 
+#if __name__ == '__main__': 
+#   main() 
 
 
